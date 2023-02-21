ac0264299ed11142280b246a66d1c1110cbc15ee
keito-isurugi <ja224kg@gmail.com>
2023-02-22 08:09:26 +0900

    コンポーネント作成、画像表示

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,17 @@
 import './bootstrap';
 
 import ReactDOM from "react-dom/client";
+import Counter from "./components/Counter";
+import TestImg from "../img/test.png";
 
 function App() {
-    return <h1>Hello World!!</h1>;
+	return (
+			<>
+					<h1>Hello World</h1>
+					<img src={TestImg} />
+					<Counter />
+			</>
+	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));

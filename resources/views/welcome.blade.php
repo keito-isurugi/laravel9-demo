5f1a3a72fd68fa59bbebbe57a64dae3be9a314da
keito-isurugi <ja224kg@gmail.com>
2023-02-22 08:14:27 +0900

    TypeScript導入

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
         @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+        @vite(['resources/css/app.css', 'resources/ts/app.tsx'])
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         react(),
         laravel({
             input: [
                 'resources/css/app.css', 
-                'resources/js/app.jsx'
+                'resources/ts/app.tsx'
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/ts',
+        },
+    },
 });
